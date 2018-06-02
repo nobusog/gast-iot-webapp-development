010048b4ab228729cec04437c90c042d60ecd14b
Okeke <EOkeke@idexcorp.com>
2018-06-02 11:10:20 -0400

    fix

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -115,8 +115,7 @@ $(document).ready(function () {
         prev_signal = 0;
         return 0;
       }
-    }
-    else {
+    } else {
       if (prev_signal = 0){
         startTime = new Date();
         prev_signal = 1;
@@ -124,7 +123,7 @@ $(document).ready(function () {
       } else {
         endTime = new Date();
         console.log(endTime);
-        time_diff = Math.round((endTime - startTime)/1000) ;
+        time_diff = (endTime - startTime)/1000 ;
         console.log(time_diff);
         prev_signal = 1;
         return time_diff;
