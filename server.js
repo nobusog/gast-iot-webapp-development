2a70d6c3a3e1afab250895ad5a0dcf87a364cc90
Okeke <EOkeke@idexcorp.com>
2018-06-13 09:33:20 -0400

    test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const iotHubClient = require('./IoThub/iot-hub.js');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', function (req, res) {
   res.render('index.html');
 });
