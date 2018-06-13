3f1f200f93c84d1f9080669cc759cc777505d493
Okeke <EOkeke@idexcorp.com>
2018-06-13 14:28:31 -0400

    add close buttons for charts

--- public/javascripts/indexEvents.js
+++ public/javascripts/indexEvents.js
@@ -3,4 +3,21 @@ $(document).ready(function () {
     document.getElementById("bme280Button").onclick = function() {
         document.getElementById("bmeContainer").classList.remove("d-none")
     };
+    document.getElementById("am2302Button").onclick = function() {
+        document.getElementById("am2302Container").classList.remove("d-none")
+    };
+    document.getElementById("pressureTransmitterButton").onclick = function() {
+        document.getElementById("pressureTransmitterContainer").classList.remove("d-none")
+    };
+
+    //close charts when button is clicked
+    document.getElementById("bme280CloseButton").onclick = function() {
+        document.getElementById("bmeContainer").classList.add("d-none")
+    };
+    document.getElementById("am2302CloseButton").onclick = function() {
+        document.getElementById("am2302Container").classList.add("d-none")
+    };
+    document.getElementById("pressureTransmitterCloseButton").onclick = function() {
+        document.getElementById("pressureTransmitterContainer").classList.add("d-none")
+    };
 })
