68a3be702e4723fda0fd0353e67a14d01a011860
Okeke <EOkeke@idexcorp.com>
2018-06-13 08:27:51 -0400

    return to last stable version

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const iotHubClient = require('./IoThub/iot-hub.js');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(function (req, res/*, next*/) {
   res.redirect('/');
 });
