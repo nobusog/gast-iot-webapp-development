2f9fca970e803204b4fe89fa697bb10da129cbb8
Okeke <EOkeke@idexcorp.com>
2018-06-13 16:46:39 -0400

    test

--- public/javascripts/indexEvents.js
+++ public/javascripts/indexEvents.js
@@ -1,4 +1,38 @@
 $(document).ready(function () {
+    //initialize map in index when button is clicked
+    document.getElementById("mapButton").onclick  = function (){
+        document.getElementById("map").classList.remove("d-none");
+        var map = new Microsoft.Maps.Map('#bingmap');
+   }
+
+    
+
+    //close map when close button is clicked 
+    document.getElementById("mapCloseButton").onclick = function() {
+        document.getElementById("map").classList.add("d-none");
+    };
+   //Insert Current Time into jumbotron
+   var currentTime =   new Date().toLocaleString();
+   document.getElementById("timePlaceHolder").innerHTML = currentTime;
+
+   //Insert Current Location into jumbotron
+   document.getElementById("locationPlaceHolder").innerHTML = "Current Location";
+
+   //Insert Current Time into jumbotron
+   var currentTimeHours = new Date().getHours();
+   
+   if (currentTimeHours < 12 & currentTimeHours >= 0) {
+       greeting = "Good Morning, ";
+   }
+
+   else if (currentTimeHours >= 12 & currentTimeHours <= 17) {
+       greeting = "Good Afternoon, ";
+   }
+   else {
+       greeting = "Good Evening, ";
+   }
+   document.getElementById("greeting").innerHTML = greeting;
+    
     //Show charts when button is clicked
     document.getElementById("bme280Button").onclick = function() {
         document.getElementById("bmeContainer").classList.remove("d-none");
@@ -10,19 +44,6 @@ $(document).ready(function () {
         document.getElementById("pressureTransmitterContainer").classList.remove("d-none");
     };
 
-    //show device map when button is clicked
-    document.getElementById("mapButton").onclick = function() {
-        document.getElementById("map").classList.remove("d-none");
-    }; 
-
-    //close map when close button is clicked 
-    document.getElementById("mapCloseButton").onclick = function() {
-        document.getElementById("map").classList.add("d-none");
-    };
-    //initialize map in index
-    function GetMap(){
-        var map = new Microsoft.Maps.Map('#map')
-   }
 
     //close charts when button is clicked
     document.getElementById("bme280CloseButton").onclick = function() {
@@ -34,27 +55,4 @@ $(document).ready(function () {
     document.getElementById("pressureTransmitterCloseButton").onclick = function() {
         document.getElementById("pressureTransmitterContainer").classList.add("d-none");
     };
-
-    //Insert Current Time into jumbotron
-    var currentTime =   new Date().toLocaleString();
-    document.getElementById("timePlaceHolder").innerHTML = currentTime;
-
-    //Insert Current Location into jumbotron
-    document.getElementById("locationPlaceHolder").innerHTML = "Current Location";
-
-    //Insert Current Time into jumbotron
-    var currentTimeHours = new Date().getHours();
-    
-    if (currentTimeHours < 12 & currentTimeHours >= 0) {
-        greeting = "Good Morning, ";
-    }
-
-    else if (currentTimeHours >= 12 & currentTimeHours <= 17) {
-        greeting = "Good Afternoon, ";
-    }
-    else {
-        greeting = "Good Evening, ";
-    }
-    document.getElementById("greeting").innerHTML = greeting;
-
 });
