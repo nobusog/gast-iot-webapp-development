4500f4a80b73e31ba359e41735da984e58d2f4bf
Okeke <EOkeke@idexcorp.com>
2018-06-15 13:57:00 -0400

    test

--- public/javascripts/indexEvents.js
+++ public/javascripts/indexEvents.js
@@ -98,7 +98,7 @@ $(document).ready(function () {
     //show which compressor is active 
     document.getElementById("jun-airSelectButton").onclick( function(){
     if (this.classList.contains("active")){
-        document.getElementById("deviceAlert").alert();
+        document.getElementById("deviceAlert").
     }
     else {
         document.getElementById("deviceSelectContainer").getElementsByClassName("active").classList.remove("active")
