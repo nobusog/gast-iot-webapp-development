e868ad47b8eba93fcdbfabd955bd840001c46234
Okeke <EOkeke@idexcorp.com>
2018-06-21 16:13:04 -0400

    test

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
     document.getElementById("settingsButton").onclick = function () {
         document.getElementById("page").classList.add("d-none");
+        document.getElementById("chartSelector").classList.add("d-none")
+    }
+
+    document.getElementById("homeButton").onclick = function () {
+        document.getElementById("page").classList.remove("d-none");
+        document.getElementById("chartSelector").classList.remove("d-none")
     }
 });
