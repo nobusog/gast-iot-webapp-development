729641ed136474f30d7b979c646592b8d52c677d
Okeke <EOkeke@idexcorp.com>
2018-06-21 16:25:44 -0400

    test

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
     document.getElementById("settingsButton").onclick = function () {
         document.getElementById("homePage").classList.add("d-none");
-        document.getElementById("chartSelector").classList.add("d-none")
+        document.getElementById("chartSelector").classList.add("d-none");
+        document.getElementById("settingspage").classList.remove("d-none");
     }
 
     document.getElementById("homeButton").onclick = function () {
         document.getElementById("homePage").classList.remove("d-none");
-        document.getElementById("chartSelector").classList.remove("d-none")
+        document.getElementById("chartSelector").classList.remove("d-none");
+        document.getElementById("settingspage").classList.add("d-none");
     }
 });
