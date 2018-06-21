b138ded4c65d06dd3109798b736624534cc6b823
Okeke <EOkeke@idexcorp.com>
2018-06-21 16:26:40 -0400

    fix

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
     document.getElementById("settingsButton").onclick = function () {
         document.getElementById("homePage").classList.add("d-none");
         document.getElementById("chartSelector").classList.add("d-none");
-        document.getElementById("settingspage").classList.remove("d-none");
+        document.getElementById("settingsPage").classList.remove("d-none");
     }
 
     document.getElementById("homeButton").onclick = function () {
         document.getElementById("homePage").classList.remove("d-none");
         document.getElementById("chartSelector").classList.remove("d-none");
-        document.getElementById("settingspage").classList.add("d-none");
+        document.getElementById("settingsPage").classList.add("d-none");
     }
 });
