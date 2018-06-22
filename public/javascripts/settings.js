d68c570a2ebd88c6d5934a2b12383969f6695edb
Okeke <EOkeke@idexcorp.com>
2018-06-22 16:18:16 -0400

    test

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -35,43 +35,50 @@ $(document).ready(function () {
         }
     }
 
+    function successfulAlert (words) {
+        document.getElementById("settingsSuccessfulAlertText").innerHTML = words
+        document.getElementById("settingsSuccessfulAlert").classList.remove("d-none")
+        setTimeout(function() {
+            document.getElementById("settingsSuccessfulAlert").classList.add("d-none")
+        },3000)
+    }
 
-        function sendData() {
-            var XHR = new XMLHttpRequest();
-      
-            // Bind the FormData object and the form element
-            var FD = new FormData(form);
-            if  (document.getElementById("settingsJunairSelectButton").classList.contains("active")) {
-                FD.append("Junair") 
-            }
-            else if (document.getElementById("settingsNitrogenSelectButton").classList.contains("active")){
-                FD.append("Nitrogen") 
-            }
-            // Define what happens on successful data submission
-            XHR.addEventListener("load", function(event) {
-                alert("Request Sent Successfully");
-            });
-        
-            // Define what happens in case of error
-            XHR.addEventListener("error", function(event) {
-                alert('Oops! Something went wrong.');
-            });
-        
-            // Set up our request
-            XHR.open("POST", "https://rasberrysensorapp.azurewebsites.net/");
-        
-            // The data sent is what the user provided in the form
-            XHR.send(FD);
-            }
-        
-            // Access the form element...
-            var form = document.getElementById("reportRequestForm");
-        
-            // ...and take over its submit event.
-            form.addEventListener("submit", function (event) {
-            event.preventDefault();
-        
-            sendData();
+    function sendData() {
+        var XHR = new XMLHttpRequest();
+    
+        // Bind the FormData object and the form element
+        var FD = new FormData(form);
+        if  (document.getElementById("settingsJunairSelectButton").classList.contains("active")) {
+            FD.append("Junair") 
+        }
+        else if (document.getElementById("settingsNitrogenSelectButton").classList.contains("active")){
+            FD.append("Nitrogen") 
+        }
+        // Define what happens on successful data submission
+        XHR.addEventListener("load", function(event) {
+            successfulAlert("Request Sent Successfully");
         });
+    
+        // Define what happens in case of error
+        XHR.addEventListener("error", function(event) {
+            alert('Oops! Something went wrong.');
+        });
+    
+        // Set up our request
+        XHR.open("POST", "https://rasberrysensorapp.azurewebsites.net/");
+    
+        // The data sent is what the user provided in the form
+        XHR.send(FD);
+        }
+    
+        // Access the form element...
+        var form = document.getElementById("reportRequestForm");
+    
+        // ...and take over its submit event.
+        form.addEventListener("submit", function (event) {
+        event.preventDefault();
+    
+        sendData();
+    });
 
 });
