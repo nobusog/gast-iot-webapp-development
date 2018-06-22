dac3c2370ac7ca311524a47b5e493c3458eb4d3a
Okeke <EOkeke@idexcorp.com>
2018-06-22 09:11:37 -0400

    test

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -6,4 +6,21 @@ $(document).ready(function () {
         document.getElementById("homeButtonContainer").classList.remove("active");
         document.getElementById("settingsButtonContainer").classList.add("active"); 
     }
+
+    document.getElementById("quickStatSettingsTab").onclick = function() {
+        hideAllTabs ();
+        document.getElementById("quickStatsSettings").classList.remove("d-none");
+    }
+
+    document.getElementById("reportsSettingsTab").onclick = function() {
+        hideAllTabs ();
+        document.getElementById("reportsSettings").classList.remove("d-none");
+    }
+
+    function hideAllTabs () {
+        tabList = getElementsByClassName("settingsTab");
+        for (var i=0; i<tabList.length; i++) {
+            tabList[i].classList.add("d-none")
+        }
+    }
 });
