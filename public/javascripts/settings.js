bbd28fbf55d96af417f0081cf0f34ccb771bd6f4
Okeke <EOkeke@idexcorp.com>
2018-06-26 10:46:30 -0400

    test

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -46,41 +46,41 @@ $(document).ready(function () {
     var form = document.getElementById("reportRequestForm");
 
     form.onsubmit = function (e) {
-    // stop the regular form submission
-    e.preventDefault();
+        // stop the regular form submission
+        e.preventDefault();
 
-    var deviceIdVal
-    var emailVal = document.getElementById("reportEmailInput").value;
-    var reportLengthVal = document.getElementById("reportLengthOptions").options[document.getElementById("reportLengthOptions").selectedIndex].value ;
-    if (document.getElementById("settingsJunairSelectButton").classList.contains("active")) {
-        console.log("i see junair");
-        deviceIdVal = "junair";
-    }
-    else if (document.getElementById("settingsNitrogenSelectButton").classList.contains("active")) {
-        console.log("i see nitrogen");
-        deviceIdVal = "nitrogen";
-    }
+        var deviceIdVal
+        var emailVal = document.getElementById("reportEmailInput").value;
+        var reportLengthVal = document.getElementById("reportLengthOptions").options[document.getElementById("reportLengthOptions").selectedIndex].value ;
+        if (document.getElementById("settingsJunairSelectButton").classList.contains("active")) {
+            console.log("i see junair");
+            deviceIdVal = "junair";
+        }
+        else if (document.getElementById("settingsNitrogenSelectButton").classList.contains("active")) {
+            console.log("i see nitrogen");
+            deviceIdVal = "nitrogen";
+        }
 
-    // construct an HTTP request
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://prod-12.northcentralus.logic.azure.com:443/workflows/187c76b9f18f48dab7a84882ca8eb0ea/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=sKpeLDmUbUw2xmdEls2ZDec-QNv6jwHTCnzUea9ahxs", true);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        // construct an HTTP request
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://prod-12.northcentralus.logic.azure.com:443/workflows/187c76b9f18f48dab7a84882ca8eb0ea/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=sKpeLDmUbUw2xmdEls2ZDec-QNv6jwHTCnzUea9ahxs", true);
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 
-    // Define what happens on successful data submission
-    xhr.addEventListener("load", function(event) {
-        successfulAlert("Request Sent Successfully");
-    });
+        // Define what happens on successful data submission
+        xhr.addEventListener("load", function(event) {
+            successfulAlert("Request Sent Successfully");
+        });
 
-    // Define what happens in case of error
-    xhr.addEventListener("error", function(event) {
-        alert('Oops! Something went wrong.');
-    });
-    console.log(JSON.stringify({"devicdeId": "junair", "email": emailVal, "reportLength": reportLengthVal}));
-    // send the collected data as JSON
-    xhr.send(JSON.stringify({"devicdeId": "junair", "email": emailVal, "reportLength": reportLengthVal}));
+        // Define what happens in case of error
+        xhr.addEventListener("error", function(event) {
+            alert('Oops! Something went wrong.');
+        });
+        console.log(JSON.stringify({"devicdeId": "junair", "email": emailVal, "reportLength": reportLengthVal}));
+        // send the collected data as JSON
+        xhr.send(JSON.stringify({"devicdeId": "junair", "email": emailVal, "reportLength": reportLengthVal}));
 
-    xhr.onloadend = function () {
-        // done
-    };
+        xhr.onloadend = function () {
+            // done
+        };
     }; 
 });
