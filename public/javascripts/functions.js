7f15fddbcc1fb3463b22104ee151c1edd7bcc511
Okeke <EOkeke@idexcorp.com>
2018-06-27 13:59:36 -0400

    test

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,17 +1,17 @@
 function chartDumper(array, str) {
-var arry = JSON.parse(window.localStorage.getItem(str));
-console.log(window.localStorage.getItem(str))
-if (arry != null){
-if (array.length <= 50) {
-    for (var n=0; n<arry.length; n++) {
-    array.push(arry.n);
+    var arry = JSON.parse(window.localStorage.getItem(str));
+    console.log(window.localStorage.getItem(str))
+    if (arry != null){
+        if (array.length <= 50) {
+            for (var n=0; n<arry.length; n++) {
+            array.push(arry[n]);
+            }
+        } else {
+            for (var n=0; n<=50; n++) {
+            array.push(arry[n]);
+            }
+        } 
     }
-} else {
-    for (var n=0; n<=50; n++) {
-    array.push(arry.n);
-    }
-} 
-}
 }
 
 function updateAllCharts (array){
@@ -21,10 +21,10 @@ function updateAllCharts (array){
   }
 
   function chartSaver (array, str) {
-    console.log(array)
     if (array != null) {
         var arrayString = JSON.stringify(array);
         console.log(arrayString);
         window.localStorage.setItem(str, arrayString);
     }
 }
+
