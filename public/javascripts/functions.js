a5cf91c8dfc217b0e60dec075448e8fb1b3d918e
Okeke <EOkeke@idexcorp.com>
2018-06-27 16:01:06 -0400

    test

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -4,8 +4,13 @@ function chartDumper(array, str) {
     var arrayString = storage.getItem(str);
     var arrayOfStrings = arrayString.split(",:");
     console.log(arrayOfStrings)
+    var n =0;
     for (var i=0; i<arrayOfStrings.length; i++) {
-        array[i] =parseInt(escapeHtml(arrayOfStrings[i]),10);
+        var number =parseInt(escapeHtml(arrayOfStrings[i]),10);
+        if (typeof number == "number") {
+            array[n] == number;
+            n++;
+        }
     }
 }
 
