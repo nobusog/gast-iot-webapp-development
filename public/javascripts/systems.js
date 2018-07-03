7382a6c9f61ecf397839172dd94389346c97a769
Okeke <EOkeke@idexcorp.com>
2018-07-03 14:42:44 -0400

    test

--- public/javascripts/systems.js
+++ public/javascripts/systems.js
@@ -6,9 +6,13 @@ $(document).ready(function () {
         document.getElementById("systemsPage").classList.remove("d-none");
         document.getElementById("nitrogenSystemContainer").classList.add("d-none");
         document.getElementById("junairSystemContainer").classList.remove("d-none");
-
     }
 
-
-
+    document.getElementById("nitrogenStateDisplay").onclick = function() {
+        document.getElementById("homePage").classList.add("d-none");
+        document.getElementById("settingsPage").classList.add("d-none");
+        document.getElementById("systemsPage").classList.remove("d-none");
+        document.getElementById("nitrogenSystemContainer").classList.remove("d-none");
+        document.getElementById("junairSystemContainer").classList.add("d-none");
+    }
 })
