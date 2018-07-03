8a4c8a1ddfc6661014d1d286794a9477b20b74a5
Okeke <EOkeke@idexcorp.com>
2018-07-03 14:45:01 -0400

    test

--- public/javascripts/systems.js
+++ public/javascripts/systems.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     document.getElementById("junairStateDisplay").onclick = function() {
         document.getElementById("homePage").classList.add("d-none");
         document.getElementById("settingsPage").classList.add("d-none");
+        document.getElementById("chartSelector").classList.add("d-none");
         document.getElementById("systemsPage").classList.remove("d-none");
         document.getElementById("nitrogenSystemContainer").classList.add("d-none");
         document.getElementById("junairSystemContainer").classList.remove("d-none");
@@ -11,6 +12,7 @@ $(document).ready(function () {
     document.getElementById("nitrogenStateDisplay").onclick = function() {
         document.getElementById("homePage").classList.add("d-none");
         document.getElementById("settingsPage").classList.add("d-none");
+        document.getElementById("chartSelector").classList.add("d-none");
         document.getElementById("systemsPage").classList.remove("d-none");
         document.getElementById("nitrogenSystemContainer").classList.remove("d-none");
         document.getElementById("junairSystemContainer").classList.add("d-none");
